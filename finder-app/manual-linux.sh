9f58fec638462a5c848e815bb40076e7a33ece3e
jaimeibanezrivera <jaimeibanezrivera@gmail.com>
2024-11-21 10:46:58 +0100

    assignment 3 part 2 finished

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -9,7 +9,7 @@ OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
-FINDER_APP_DIR="/home/jaumas/UNI/Boulder_Embedded_Linux/ASSIGNMENTS/assignments-3-and-later-jaimeibanezrivera/finder-app"
+FINDER_APP_DIR=$(realpath $(dirname $0))
 echo "FINDER_APP_DIR is set to: ${FINDER_APP_DIR}"
 
 
